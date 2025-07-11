9775a611a7f878bcfa29dd24e1334bb2388f6e4b
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-12 02:44:51 +0300

    Question 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using System.ComponentModel;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -10,6 +12,17 @@
         public static void IncrementReference(ref int value)
         {
             value++;
+        }
+        #endregion
+
+        #region Question (2)
+        public static void ModifyStringVal(string str)
+        {
+            str = "Ali"; 
+        }
+        public static void ModifyStringRef(ref string str)
+        {
+            str = "Sayed";
         }
         #endregion
         static void Main(string[] args)
@@ -26,6 +39,23 @@
             IncrementReference(ref num2);
             Console.WriteLine($"After Increment(Passing by value): {num1}");
             Console.WriteLine($"After Increment(Passing by reference): {num2}");
+            #endregion
+
+            #region Question (2)
+            //2 - Explain the difference between passing(Reference type
+            //parameters) by value and by reference then write a suitable c#
+            //example.
+
+            // (Answer) => => => =>
+            // Passing a reference type by value means that a copy of the reference is passed, allowing the method to modify the original object but not the reference itself.
+            // Passing a reference type by reference means that the method can modify both the object and the reference itself.
+            string name1 = "Ahmed";
+            string name2 = "Omar";
+            ModifyStringVal(name1);
+            ModifyStringRef(ref name2);
+            Console.WriteLine($"Ahmed After ModifyString(Passing by value): {name1}");
+            Console.WriteLine($"Omar After ModifyString(Passing by reference): {name2}");
+
             #endregion
         }
     }
