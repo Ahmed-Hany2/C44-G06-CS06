785edb50f275bccf13a0d49e8a2a2b3892502383
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-12 15:45:59 +0300

    Functions (prototype)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,11 +2,27 @@
 {
     internal class Program
     {
-        
+        #region Functions (prototype)
+        public static void PrintShape1()
+        {
+            Console.WriteLine("$_");
+        }
+        public static void PrintShape2(string shape ,int count =10) 
+        { 
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine(shape);
+            }
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            
 
+            #region Functions (prototype)
+            PrintShape1();
+            PrintShape2(count: 20, shape: "%^");
+            #endregion
 
         }
     }
